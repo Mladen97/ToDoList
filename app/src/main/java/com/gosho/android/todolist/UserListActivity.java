13b34ddd587461d3819241407af87627e07e2eaf
Mladen Filipov <mladen4o2006@abv.bg>
2016-06-27 15:48:54 +0300

    no message

--- app/src/main/java/com/gosho/android/todolist/UserListActivity.java
+++ app/src/main/java/com/gosho/android/todolist/UserListActivity.java
@@ -8,6 +8,6 @@ public class UserListActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_userList);
+        setContentView(R.layout.activity_user_list);
     }
 }
